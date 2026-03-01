26a570e88f48156f449103bbe49efd13e2f2a09f
guimard11 <guimardsyvrain@gmail.com>
2026-03-01 15:15:31 -0500

    Creation of the Silver to Gold notebook to process data stored in the lakehouse Silver.

---
+++ NB_Silver_to_Gold_Transformation.Notebook/notebook-content.py
@@ -0,0 +1,37 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "ad6c2670-8e52-4cb1-a385-669f61c55b3f",
+# META       "default_lakehouse_name": "LH_success_silver",
+# META       "default_lakehouse_workspace_id": "32baed3b-fac8-4061-9887-3b7530630ba4",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "ad6c2670-8e52-4cb1-a385-669f61c55b3f"
+# META         },
+# META         {
+# META           "id": "f6edd3ff-48af-414c-b2a8-1b0ced2322dd"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
